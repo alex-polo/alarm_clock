cfb3c00dd271090aad64982637877b67db51a2ce
Apolo <polonnikov.alexander@gmail.com>
2026-01-24 22:34:33 +0300

    feat: introduce run.py as application entry point

--- run.py
+++ run.py
@@ -1,5 +1,17 @@
-from src import start_clock
+import logging
 
+from src import start_clock
+from src.conf import setup_logging
+
+setup_logging()
+
+log = logging.getLogger(__name__)
 
 if __name__ == "__main__":
-    start_clock()
+    try:
+        log.info("Starting...")
+        start_clock()
+    except KeyboardInterrupt:
+        log.info("Done.")
+    except Exception:
+        log.exception("Error.")
